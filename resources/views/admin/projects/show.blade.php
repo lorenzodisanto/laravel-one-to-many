23fc29881f6057caf6a60363cad5c72ec2e92c6b
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-04 16:35:16 +0200

    feat: add type in project details

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -17,6 +17,7 @@
                 <div class="card-body">
                     <h2>{{ $project->id }} - {{ $project->title }}</h2>
                     <code>{{ $project->slug }}</code>
+                    <p class="fs-5"><b>Type:</b> {{ $project->type->label }}</p>
                     <p class="fs-5">{{ $project->description }}</p>
                     <a href="{{ $project->link }}">link</a>
                 </div>
